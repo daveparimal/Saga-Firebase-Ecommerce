fe8f57de8486fff7664129ce29230d847c664103
Parimal Dave <parimal.s.dave@gmail.com>
2021-03-12 16:01:41 +0530

    react router, layouts, footer and components

--- Frontend_Code/e-commerce/src/components/Header/index.js
+++ Frontend_Code/e-commerce/src/components/Header/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import './styles.scss'
+import './styles.scss';
+import { Link } from 'react-router-dom';
 
 import logo from './../../assets/logo.png';
 
@@ -8,7 +9,19 @@ const Header = props => {
         <header className="header">
             <div className="wrap">
                 <div className="logo">
-                    <img src={logo} alt="Ecommerce Logo" />
+                    <Link to="/">
+                        <img src={logo} alt="Ecommerce Logo" />
+                    </Link>
+                </div>
+                
+                <div className="callToActions">
+                    <ul>
+                        <li>
+                            <Link to="/registration">
+                                Register
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
             </div>
 
